a5e154f00b2daad9cea2df4430b6ed23662c928b
Connor Harris <connorharris@Connors-MacBook-Pro-2.local>
2022-10-31 11:22:46 -0400

    ahhh

--- seed/data.js
+++ seed/data.js
@@ -3,7 +3,7 @@ import Player from "../models/Player.js"
 import players from "./Players.json" assert {type: "json"}
 
 const insertData = async () => {
-  await db.dropDatabase()
+  await Player.deleteMany({})
   await Player.create(players)
   db.close()
 }
